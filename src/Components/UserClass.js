f71bf347b3a01b9b33569c28a7dd00a29d680bbb
Awnish462 <awnishsinha462@gmail.com>
2025-05-11 15:51:12 +0530

    using this.state and this.setState for rendering data

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 class User extends React.Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
 
   render() {
     return (
@@ -11,6 +14,16 @@ class User extends React.Component {
         <h1>{this.props.name}</h1>
         <h2>{this.props.area}</h2>
         <h2>{this.props.work}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          count
+        </button>
+        <h1>{this.state.count}</h1>
       </div>
     );
   }
